d427574cc20b9cf26f0e536a7a76c1c781c107e6
myfocus <431027103@qq.com>
2016-08-20 14:27:02 +0800

    @20160820

--- sj/js/index.js
+++ sj/js/index.js
@@ -112,3 +112,12 @@
 			})
 		});
 			*/
+
+		$(document).ready(function() {
+			$('.skill_navbox li').on('click', function(event) {
+				alert(111)
+				$(this).addClass('active').siblings().removeClass('active');
+				$('.skill_tit').eq($(this).index()).addClass('on').siblings().removeClass('on');
+				/* Act on the event */
+			});
+		});
